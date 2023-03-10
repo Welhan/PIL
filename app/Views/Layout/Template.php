a4689afb45bd138a4d83c3465b5ee838708a6cb7
Welhan <welhan.susanto@gmail.com>
2023-03-10 12:07:19 +0700

    mengganti icon setting menjadi gear

--- app/Views/Layout/Template.php
+++ app/Views/Layout/Template.php
@@ -35,7 +35,7 @@ $topmenu = generateOtherMenu();
         <?php if ($topmenu) : ?>
             <ul class="navbar-nav ms-auto me-0 me-md-3 my-2 my-md-0">
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-gear fa-fw"></i></a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
 
                         <?php foreach ($topmenu as $top) : ?>
